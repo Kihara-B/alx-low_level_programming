864cb0a81fbc4c05851e12302fa4fe4db41e43fa
Kihara-B <kiharabrian1@gmail.com>
2022-12-15 00:48:00 +0300

    0-putchar task

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,60 @@
+#lfndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+main.h[+] [unix] (02:59 01/01/1970)                                                  11,25 All
+-- INSERT --
+
