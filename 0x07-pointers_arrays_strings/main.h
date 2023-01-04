78235f3808137eca2a3a9391c7e6d9c8ed89258e
Kihara-B <kiharabrian1@gmail.com>
2023-01-05 01:43:06 +0300

    5-strstr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef main_h
+#define main_h
 
-/**
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x06-pointers_arrays_strings directory.
- */
 
 #include <stddef.h>
 
