b57e5499b7e7a23d2a837139f1dc95660615ce83
Kihara-B <kiharabrian1@gmail.com>
2023-01-07 04:14:48 +0300

    task3

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,14 @@
-#include "main.h"
-
-/**
- * _puts_recursion - Prints a string, followed by a new line.
- * @s: The string to be printed.
- */
-void _puts_recursion(char *s)
-{
-        if (*s)
-        {
-                _putchar(*s);
-                _puts_recursion(s + 1);
-        }
-
-        else
-                _putchar('\n');
+#include "main.h"                                                                                                                       
+                                                                                                                                        
+/**                                                                                                                                     
+ * _print_rev_recursion - Prints a string in reverse.                                                                                   
+ * @s: The string to be printed.                                                                                                        
+ */                                                                                                                                     
+void _print_rev_recursion(char *s)                                                                                                      
+{                                                                                                                                       
+        if (*s)                                                                                                                         
+        {                                                                                                                               
+                _print_rev_recursion(s + 1);                                                                                            
+                _putchar(*s);                                                                                                           
+        }                                                                                                                               
 }
