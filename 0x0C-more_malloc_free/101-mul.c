3aa113a85551a34661a378fa594a79c29356397c
Kihara-B <kiharabrian1@gmail.com>
2023-01-19 00:37:14 +0300

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,11 +3,12 @@
 #include "main.h"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+* is_digit - checks if a string contains a non-digit char
  *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
+* @s: string to be evaluated
+*
+* Return: returns 0 if a non-digit is found, 1 otherwise
+*/
 int is_digit(char *s)
 {
 	int i = 0;
@@ -22,11 +23,12 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
+* _strlen - returns the length of a string
+*
+* @s: string to evaluate
+*
+* Return: the length of the string
+*/
 int _strlen(char *s)
 {
 	int i = 0;
@@ -39,8 +41,8 @@ int _strlen(char *s)
 }
 
 /**
- * errors - handles errors for main
- */
+* errors - handles errors for main
+*/
 void errors(void)
 {
 	printf("Error\n");
@@ -48,12 +50,12 @@ void errors(void)
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
- */
+* main - multiplies two positive numbers
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: always 0 (Success)
+*/
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
